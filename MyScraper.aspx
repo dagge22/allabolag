daed41fcd582e6001e217f8dcf5aff841957debf
dagge22 <dag.w@hotmail.com>
2014-10-03 14:26:41 +0200

    Hej

---
+++ MyScraper.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MyScraper.aspx.cs" Inherits="HTMLAgilityPackScreenScraper.MyScraper" %>
+
+<!DOCTYPE html> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+   <head runat="server"> <title></title> 
+   </head> 
+   <body>     
+      <form id="form1" runat="server"> 
+         <div> 
+	    <asp:Label ID="InputHereLabel" runat="server" Text="Skriv in organisationNR" /> 
+	    <asp:TextBox ID="InputTextBox" runat="server" /> <br /> 
+	    <asp:Button ID="ClickMeButton" runat="server" Text="Click Me" OnClick="ClickMeButton_Click" /> <br /> <br /> 
+            <asp:Label ID="OutputLabel" runat="server" /> 
+	</div> 
+      </form> 
+   </body> 
+</html> 
---
+++ MyScraper.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+
+namespace HTMLAgilityPackScreenScraper
+{
+    public partial class MyScraper : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            var getHtmlWeb = new HtmlWeb();
+	        var document = getHtmlWeb.Load("http://www.allabolag.se/" + InputTextBox.Text);
+            var span = document.DocumentNode.SelectNodes("//span");
+            var td = document.DocumentNode.SelectNodes("//td");
+            var metaTags = document.DocumentNode.SelectNodes("//meta");
+	        int counter = 1;
+ 
+            //if (metaTags != null)
+            //{
+            //    foreach (var tag in metaTags)
+            //    {
+            //        if (tag.Attributes["name"] != null && tag.Attributes["content"] != null)
+            //        {
+            //            OutputLabel.Text = tag.Attributes["name"].Value + "<br>";
+            //            OutputLabel.Text = tag.Attributes["content"].Value + "<br>";
+
+            //        }
+            //    }
+            //}
+
+            if (span != null)
+            {
+                foreach (var spans in span.Where(tag => tag.Id == "printTitle"))
+                {
+                    foreach (var tag in span.Where(tag => tag.Id == "printOrgnr"))
+                    {
+                        OutputLabel.Text += counter + ". " + spans.InnerHtml + "\t" + "<br>";
+                        counter++;
+                        OutputLabel.Text += counter + ". " + tag.InnerHtml + "\t" + "<br>";
+                        counter++;
+
+                    }
+                }
+            }
+
+        
+      }
+        }
+    }
